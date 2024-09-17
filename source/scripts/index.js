72c4c8a43194a05849907d73dff52cb842b9be83
realisereallies <dav.liza@mail.ru>
2024-09-17 12:32:29 +0300

    добавляет слайдер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,4 +1,5 @@
 const button = document.querySelector('.main-nav__toggle');
 button.onclick = function () {
   document.querySelector('.main-nav__list').classList.toggle('main_nav-close');
+  button.classList.toggle('checked');
 };
