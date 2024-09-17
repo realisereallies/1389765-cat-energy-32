5e6e811d8107360539abd666d0c2bfc41fc469df
realisereallies <163350360+realisereallies@users.noreply.github.com>
2024-09-17 09:36:07 +0000

    Deploying to gh-pages from @ realisereallies/1389765-cat-energy-32@b3122ac85044fa73c41137385ab6367109ad02bc 🚀

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,1 @@
-var n=document.querySelector(".main-nav__toggle");n.onclick=function(){document.querySelector(".main-nav__list").classList.toggle("main_nav-close")};
+var e=document.querySelector(".main-nav__toggle");e.onclick=function(){document.querySelector(".main-nav__list").classList.toggle("main_nav-close"),e.classList.toggle("checked")};
