b70597b92b560252d5f7c535e6049767de588fe6
realisereallies <163350360+realisereallies@users.noreply.github.com>
2024-09-24 17:47:58 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,7 @@
-var e=document.querySelector(".main-nav__toggle");e.onclick=function(){document.querySelector(".main-nav__list").classList.toggle("main_nav-close"),e.classList.toggle("checked")};
+// source/scripts/index.js
+var button = document.querySelector(".main-nav__toggle");
+button.onclick = function() {
+  document.querySelector(".main-nav__list").classList.toggle("main_nav-close");
+  button.classList.toggle("checked");
+};
+//# sourceMappingURL=index.js.map
