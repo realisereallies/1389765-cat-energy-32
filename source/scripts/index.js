887142552b664cd152c83831d527c693e66b6475
realisereallies <163350360+realisereallies@users.noreply.github.com>
2024-09-28 19:58:24 +0300

    фиксит сетки2 (#60)
    * фиксит сетки
    
    * фиксит сетки
    
    * фиксит сетки
    
    * фиксит сетки
    
    * фиксит сетки
    
    * фиксит сетки1

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,5 +1,5 @@
 const button = document.querySelector('.main-nav__toggle');
 button.onclick = function () {
-  document.querySelector('.main-nav__list').classList.toggle('main_nav-close');
+  document.querySelector('.main-nav__list').classList.toggle('main-nav-close');
   button.classList.toggle('checked');
 };
